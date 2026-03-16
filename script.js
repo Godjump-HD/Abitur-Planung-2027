aa15275b2fecc2614df8384e614d59a6867305a3
Tim Kutschker <lostixd8@gmail.com>
2026-03-16 12:08:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function test(){
   const user = localStorage.getItem("abi_user");
-  const btn = document.getElementById("testUser").innerText += user.name;
+  const btn = document.getElementById("testUser").innerText += user.name();
 }
