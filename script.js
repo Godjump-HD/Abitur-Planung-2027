782c9122497f023319ea804a4b15f9f01b90b262
Tim Kutschker <lostixd8@gmail.com>
2026-03-16 11:47:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function test(){
   const user = localStorage.getItem("abi_user");
   const btn = document.getElementById("loginBtn").innerText = user;
-  body.background = "red";
+  body.style.background = "red";
 }
